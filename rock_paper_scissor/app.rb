2b7f8599145050ff9d4817a737785f43a10bad79
ajosepha <ariel.isaacs@gmail.com>
2013-12-10 14:27:38 -0500

    delete game.rb file

--- rock_paper_scissor/app.rb
+++ rock_paper_scissor/app.rb
@@ -5,30 +5,31 @@ module Game
 
         get '/throw/:type' do
             computer_move = ["rock", "paper", "scissor"].sample
+
             
             if params[:type] == "rock"
                 if computer_move == "paper"
-                    "too bad for you, computer wins this round"
+                    "Computer move is #{computer_move}. too bad for you, computer wins this round"
                 elsif computer_move == "scissor"
-                     "congrats you beat the computer"
+                     "Computer move is #{computer_move}. congrats you beat the computer"
                 else
-                      "it's a tie"
+                      "Computer move is #{computer_move}. it's a tie"
                 end
             elsif params[:type] == "paper"
                 if computer_move == "scissors"
-                    "too bad for you, computer wins this round"
+                    "Computer move is #{computer_move}. too bad for you, computer wins this round"
                 elsif computer_move == "rock"
-                 "congrats you beat the computer"
+                    "Computer move is #{computer_move}. congrats you beat the computer"
                 else 
-                 "it's a tie"
+                    "Computer move is #{computer_move}. it's a tie"
                 end
             elsif params[:type] == "scissors"
                  if computer_move == "rock"
-                     "too bad for you, computer wins this round"
+                     "Computer move is #{computer_move}. too bad for you, computer wins this round"
                 elsif computer_move == "paper"
-                    "congrats you beat the computer"
+                    "Computer move is #{computer_move}. congrats you beat the computer"
                 else
-                    "it's a tie! would you like to try again?"
+                    "Computer move is #{computer_move}. it's a tie! would you like to try again?"
                 end
             else
                 "not a valid answer"
--- rock_paper_scissor/game.rb
+++
@@ -1,9 +0,0 @@
-require 'sinatra/base'
-
-module Game
-    class RPS_App < Sinatra::Application
-        get '/throw' do
-            "hello"
-    end
-
-end
